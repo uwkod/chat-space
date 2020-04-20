056ccd95865c4a722e2a5617284ddc1a52526053
kodai fukui <kodai1863@gmail.com>
2020-04-20 17:07:02 +0900

    編集しました

--- Gemfile
+++ Gemfile
@@ -55,3 +55,10 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
  gem 'haml-rails'
  gem 'devise'
  gem 'pry-rails'
+ gem 'carrierwave'
+ gem 'mini_magick'
+ gem 'rspec-rails', '~> 3.5'
+ gem 'rails-controller-testing'
+ gem 'factory_bot_rails'
+ gem 'faker'
+
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  def index
+  end
 
   def new
     @group = Group.new
@@ -21,7 +23,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group.id), notice: 'グループを更新しました'
     else
       render :edit
     end
