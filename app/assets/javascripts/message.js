9cc321ef1f36c423d5cdf2e954539544146021fa
kodai fukui <kodai1863@gmail.com>
2020-04-27 12:40:54 +0900

    実装しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,9 +36,10 @@ $(function(){
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+    
+      url: "api/messages",
+      type: "get",
+      data: {id: last_message_id},
       dataType: 'json',
       processData: false,
       contentType: false
@@ -54,5 +55,29 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-  });
-});
+    funciton reloadMessages(){
+        var reloadMessages = function() {
+          var last_message_id = $('.message:last').data("message-id");
+          $.ajax({
+            url: "api/messages",
+            type: 'get',
+            dataType: 'json',
+            data: {id: last_message_id}
+          })
+          .done(function(messages) {
+            if (messages.length !== 0) {
+            var insertHTML = '';
+            $.each(messages, function(i, message) {
+              insertHTML += buildHTML(message)
+            });
+            $('.messages').append(insertHTML);
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+          }
+          })
+          .fail(function() {
+            alert('error');
+          });
+        };
+        if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+        setInterval(reloadMessages, 7000);
+      });
