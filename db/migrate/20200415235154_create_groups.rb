3dcca1194cd33ca144d4abc59886e96161917c79
kodai fukui <kodai1863@gmail.com>
2020-04-28 19:58:51 +0900

    edit groups_migration__file

--- db/migrate/20200415235154_create_groups.rb
+++ db/migrate/20200415235154_create_groups.rb
@@ -1,4 +1,4 @@
-class CreateGropus < ActiveRecord::Migration[5.0]
+class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
